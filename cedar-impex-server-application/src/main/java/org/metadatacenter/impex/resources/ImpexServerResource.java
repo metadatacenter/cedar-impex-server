0b026cda80d8d32d649a5baccfaee9c905e1ce66
Attila L. Egyedi <egyedia@stanford.edu>
2022-11-15 15:34:31 -0800

    Upgrade jaxb

--- cedar-impex-server-application/src/main/java/org/metadatacenter/impex/resources/ImpexServerResource.java
+++ cedar-impex-server-application/src/main/java/org/metadatacenter/impex/resources/ImpexServerResource.java
@@ -35,7 +35,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXBException;
+import jakarta.xml.bind.JAXBException;
 import java.io.FileInputStream;
 import java.io.IOException;
 
